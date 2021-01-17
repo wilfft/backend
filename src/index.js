f549c7886bda0e8236b7d273fc0188c5f2d8227a
wilfft <68745408+wilfft@users.noreply.github.com>
2021-01-17 06:26:13 -0300

    upppping to heroku

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 2000;
 app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
 app.use(routes);
 
-axios.defaults.baseURL = "http://api.github.com";
+axios.defaults.baseURL = "https://api.github.com";
 
 axios.defaults.headers.common["Authorization"] =
   "token 7de336784780ddbd223ba9e07078ed715d04098d";
