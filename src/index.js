4f0de766d42d6835e61196465a6dfa694fdbb12f
wilfft <68745408+wilfft@users.noreply.github.com>
2021-01-17 06:01:05 -0300

    upppping to heroku

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const routes = require("./routes");
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 2000;
+app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
+app.use(routes);
+
+axios.defaults.baseURL = "https://api.github.com";
+
+axios.defaults.headers.common["Authorization"] =
+  "token 7de336784780ddbd223ba9e07078ed715d04098d";
---
+++ src/routes.js
@@ -0,0 +1,59 @@
+const { Router, json } = require("express");
+const axios = require("axios");
+const routes = Router();
+
+/* 
+<https://api.github.com/users?per_page=10&since=19>; rel="next", 
+<https://api.github.com/users{?since}>; rel="first"
+*/
+///retrieves ALL USERS
+routes.get("/api/users", async (req, res) => {
+  const since = req.query.since;
+  await axios
+    .get(`/users?per_page=10&since=${since}`)
+    .then((response) => {
+      const users = response.data;
+      const filteredUsers = users.map((e) => {
+        return { id: e.id, login: e.login };
+      });
+      return res.json(filteredUsers);
+    })
+
+    .catch((err) => console.error(err));
+});
+
+///retrieveng repositories
+routes.get("/api/users/:username/repos", async (req, res) => {
+  const username = req.params.username;
+  await axios
+    .get(`/users/${username}/repos?type=owner`)
+    .then((response) => {
+      const repos = response.data;
+      const filteredRepos = repos.map((e) => {
+        return { id: e.id, name: e.name, url: e.html_url };
+      });
+      return res.json(filteredRepos);
+    })
+    .catch((err) => console.error(err));
+});
+
+//retrieves DETAILS of user
+routes.get("/api/users/:username/details", async (req, res) => {
+  const username = req.params.username;
+  const response = await axios.get(`/users/${username}`);
+  const user = {
+    id: response.data.id,
+    login: response.data.login,
+    html_url: response.data.html_url,
+    created_at: response.data.created_at,
+  };
+  return res.json(user);
+});
+
+module.exports = routes;
+/**api
+  .get("/users/wilfft")
+  .then((response) => console.log(response.data))
+  .catch((err) => {
+    console.error("ops! ocorreu um erro" + err);
+  }); */
